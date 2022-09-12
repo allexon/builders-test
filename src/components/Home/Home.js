dda083d42095e70ba582f9896140cd17176d312d
Alexon Moreira <alexon@gmail.com>
2022-09-11 21:11:02 -0300

    ajustes...

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,10 @@
 import React, {useEffect} from 'react'
 import { CardTemperature } from '../../components/index'
-import { storeAddress, storeTemperature, storeLatLon, storeModal } from '../../store/index'
+import { storeModal } from '../../store/index'
 import { Layout, Loader } from '../../default/index'
 
 const Home = () => {
 
-    const address = storeAddress(state => state)
-    const temperature = storeTemperature(state => state)
-    const latLon = storeLatLon(state => state)
     const modal = storeModal(state => state)
         
     useEffect(() => {
